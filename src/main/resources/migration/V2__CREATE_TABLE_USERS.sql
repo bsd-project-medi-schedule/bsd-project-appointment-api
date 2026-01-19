7c7747385ac8e3156636be7999640bf86151159a
BYCKILIE <tudorovidiub@gmail.com>
2026-01-20 01:01:44 +0200

    up

--- src/main/resources/migration/V2__CREATE_TABLE_USERS.sql
+++ src/main/resources/migration/V2__CREATE_TABLE_USERS.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     id               UUID PRIMARY KEY,
-    email            TEXT UNIQUE NOT NULL
+    email            TEXT UNIQUE NOT NULL,
+    role             INT NOT NULL
 );
